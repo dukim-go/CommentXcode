f1376b46cfc0db32675809d598e72519c11edc6a
daeung Kim <dwkim@gpmobile.co.kr>
2019-07-23 10:32:49 +0900

    start
    [RV] n/a

---
+++ CommentXcode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CommentXcode
+//
+//  Created by ahnlab_dukim on 23/07/2019.
+//  Copyright © 2019 daeung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CommentXcode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CommentXcode
+//
+//  Created by ahnlab_dukim on 23/07/2019.
+//  Copyright © 2019 daeung. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
