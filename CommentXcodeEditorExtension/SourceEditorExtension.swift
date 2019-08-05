437f571c91e57f93ef1a637e3a1b018313ddc0bb
daeung Kim <dwkim@gpmobile.co.kr>
2019-08-05 23:18:27 +0900

    add extension target

---
+++ CommentXcodeEditorExtension/SourceEditorExtension.swift
@@ -0,0 +1,22 @@
+//
+//  SourceEditorExtension.swift
+//  CommentXcodeEditorExtension
+//
+//  Created by daeung Kim on 2019/08/04.
+//  Copyright © 2019 daeung. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+    
+    func extensionDidFinishLaunching() {
+         //If your extension needs to do any work at launch, implement this optional method.
+    }
+    
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+}
